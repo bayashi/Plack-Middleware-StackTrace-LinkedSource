836d9e2f3a3b0a946fd57ed9ae9a6d691bdae856
bayashi <bayashi@cpan.org>
2016-02-19 23:05:51 +0900

    bump version

--- lib/Plack/Middleware/StackTrace/LinkedSource.pm
+++ lib/Plack/Middleware/StackTrace/LinkedSource.pm
@@ -11,7 +11,7 @@ use Plack::Util::Accessor qw/
     force
 /;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 sub prepare_app {
     my ($self) = @_;
@@ -136,7 +136,7 @@ see more configurations on L<Plack::Middleware::StackTrace>
 
 =begin html
 
-<a href="http://travis-ci.org/bayashi/Plack-Middleware-StackTrace-LinkedSource"><img src="https://secure.travis-ci.org/bayashi/Plack-Middleware-StackTrace-LinkedSource.png?_t=1455865821"/></a> <a href="https://coveralls.io/r/bayashi/Plack-Middleware-StackTrace-LinkedSource"><img src="https://coveralls.io/repos/bayashi/Plack-Middleware-StackTrace-LinkedSource/badge.png?_t=1455865821&branch=master"/></a>
+<a href="http://travis-ci.org/bayashi/Plack-Middleware-StackTrace-LinkedSource"><img src="https://secure.travis-ci.org/bayashi/Plack-Middleware-StackTrace-LinkedSource.png?_t=1455890721"/></a> <a href="https://coveralls.io/r/bayashi/Plack-Middleware-StackTrace-LinkedSource"><img src="https://coveralls.io/repos/bayashi/Plack-Middleware-StackTrace-LinkedSource/badge.png?_t=1455890721&branch=master"/></a>
 
 =end html
 
